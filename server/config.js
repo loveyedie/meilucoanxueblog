8b941a25908b755d9267f129e318b40b5e0c4454
Administrator <loveyedie@163.com>
2017-12-10 03:25:24 +0800

    第一次提交

--- server/config.js
+++ server/config.js
@@ -11,9 +11,9 @@ var Config = {
         cookieSecret: 'meiluocanxueblogsecret',
         name:'loveyedie',
         host:'www.loveyedie.xin',
-        url:'mongodb://root:loveyedie@dds-bp1ddbe00dfdb9041.mongodb.rds.aliyuncs.com:3717,dds-bp1ddbe00dfdb9042.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-4835041'
+        //url:'mongodb://root:loveyedie@dds-bp1ddbe00dfdb9041.mongodb.rds.aliyuncs.com:3717,dds-bp1ddbe00dfdb9042.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-4835041'
         //mongodb://root:****@dds-bp1ddbe00dfdb9041.mongodb.rds.aliyuncs.com:3717,dds-bp1ddbe00dfdb9042.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-4835041
-        //url: 'mongodb://127.0.0.1:27017/loveyedie'
+        url: 'mongodb://127.0.0.1:27017/loveyedie'
     },
     constant:{
         flash:{
